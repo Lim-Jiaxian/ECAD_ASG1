9ed9ead411ef570e8caf2b04d7cb2c5adbd5a3db
tanyipzhen <nehz@duck.com>
2025-01-26 00:26:34 +0800

    Add checkout functionality

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
 
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+$base_url = $protocol . $_SERVER['HTTP_HOST'];
+
 return (object) array(
-    'SITE_ROOT' => "/" . basename(__DIR__)
+    'SITE_ROOT' => "/" . basename(__DIR__),
+    'SITE_NAME' => "Little Wonders",
+    'SITE_URL' => $base_url
 );
--- php/checkLogin.php
+++ php/checkLogin.php
@@ -1,8 +1,6 @@
 <?php
 // Detect the current session
 session_start();
-// Include the Page Layout header
-include("header.php"); 
 
 // Reading inputs entered in previous page
 $email = $_POST["email"];
@@ -13,15 +11,13 @@ include_once("./mysql_conn.php"); // sql connection string php file
 $qry = "SELECT * FROM shopper WHERE Email LIKE '%$email%'";
 $result = $conn->query($qry);
 
-
 // To Do 1 (Practical 2): Validate login credentials with database
 if ($result->num_rows > 0) {
 	// Save user's info in session variables
-	while ($row = $result->fetch_array()){
+	while ($row = $result->fetch_array()) {
 		//Get the hashed password from database
 		$hashed_pwd = $row["Password"];
-		if (password_verify($pwd, $hashed_pwd) == true)
-		{//if (password_verify($pwd, $hashed_pwd) == true)
+		if (password_verify($pwd, $hashed_pwd)) {
 			$checkLogin = true;
 			$_SESSION["ShopperName"] = $row["Name"];
 			$_SESSION["ShopperID"] = $row["ShopperID"];
@@ -40,18 +36,15 @@ if ($result->num_rows > 0) {
 			// Redirect to home page
 			header("Location: ../index.php");
 			// Redirect to product page temporarily until home discounts is added
-			header("Location: ./category.php");
+			// header("Location: ./category.php");
 			exit;
-		}
-		else {
+		} else {
 			echo  "<h3 style='color:red'>Invalid Login Credentials</h3>";
 		}
-		
 	}
-}else{
+} else {
 	echo  "<h3 style='color:red'>Invalid User</h3>";
 }
-	
+
 // Include the Page Layout footer
 include("footer.php");
-?>
