000fa849b542304e28c1911691bf224ee89d9239
Lim Jia Xian <limjiaxian2@gmail.com>
2025-01-26 13:45:36 +0800

    Updated unhashed passwords for previous users to be hashed after successful login

--- php/checkLogin.php
+++ php/checkLogin.php
@@ -18,35 +18,43 @@ $result = $conn->query($qry);
 if ($result->num_rows > 0) {
 	// Save user's info in session variables
 	while ($row = $result->fetch_array()){
-		//Get the hashed password from database
+		// Get the hashed password or non-hashed password from database
 		$hashed_pwd = $row["Password"];
-		if (password_verify($pwd, $hashed_pwd) == true)
-		{//if (password_verify($pwd, $hashed_pwd) == true)
+		// Check if the current passowrd length is not hashed and contains less than 60 characters 
+		// Bcrypt hashed passwords contain 60 characters
+		if (strlen($hashed_pwd) < 60) {
+			if ($pwd == $hashed_pwd){
+				$checkLogin = true;
+				$_SESSION["ShopperName"] = $row["Name"];
+				$_SESSION["ShopperID"] = $row["ShopperID"];
+				// Create a password hash using the default bcrypt algorithm since the user's password was not hashed before
+				$password =  password_hash($_POST["password"], PASSWORD_DEFAULT);
+				// Define the UPDATE SQL statement for password
+				$qry = "UPDATE Shopper set Password = ? where Email = ?";
+				$stmt = $conn->prepare($qry);
+				// Binding of parameters
+				$stmt->bind_param("ss", $password, $row["Email"]);
+				// Execute SQL statement
+				$stmt->execute();
+				// Redirect to home page
+				header("Location: ../index.php");
+				exit;
+			}
+		}
+		else if (password_verify($pwd, $hashed_pwd) == true)
+		{
+			// Set checking for login to true
 			$checkLogin = true;
+			// Assign shopper details to session
 			$_SESSION["ShopperName"] = $row["Name"];
 			$_SESSION["ShopperID"] = $row["ShopperID"];
-			// // To Do 2 (Practical 4): Get active shopping cart
-			// $sid = $_SESSION["ShopperID"];
-			// $qry = "SELECT sc.ShopCartID, COUNT(sci.ProductID) AS NumItems FROM ShopCart sc LEFT JOIN ShopCartItem sci ON sc.ShopCartID = sci.ShopCartID WHERE sc.ShopperID=$sid AND sc.OrderPlaced = 0";
-			// $result = $conn->query($qry);
-
-			// if ($result->num_rows > 0) {
-			// 	while ($row = $result->fetch_array()){
-			// 		$_SESSION["Cart"] = $row["ShopCartID"];
-			// 		$_SESSION["NumCartItem"] = $row["NumItems"];
-			// 	}
-			// }
-
 			// Redirect to home page
 			header("Location: ../index.php");
-			// Redirect to product page temporarily until home discounts is added
-			header("Location: ./category.php");
 			exit;
 		}
 		else {
 			echo  "<h3 style='color:red'>Invalid Login Credentials</h3>";
 		}
-		
 	}
 }else{
 	echo  "<h3 style='color:red'>Invalid User</h3>";
